7535af318d03a6bd950347e518362bce8d9968c5
Robin Schneider <ypid23@aol.de>
2014-07-25 22:38:01 +0200

    Wrote test suite.

--- cdcat/setup.py
+++ cdcat/setup.py
@@ -4,14 +4,13 @@
 from setuptools import setup
 
 setup(
-    name         = "cdcat-parser",
+    name         = "cdcat",
     version      = "0.1",
     description  = "Use the database from cdcat to copy wanted files.",
     author       = "Robin `ypid` Schneider",
     author_email = "ypid23@aol.de",
     url          = "https://github.com/ypid/scripts/tree/master/cdcat",
-    # packages     = ["osm_time"],
-    # package_dir  = {"osm_time": "osm_time/"},
+    packages     = ["cdcat"],
     license      = "GPLv3",
     test_suite   = "tests",
 )
--- cdcat/tests/tests.py
+++ cdcat/tests/tests.py
@@ -1,22 +1,82 @@
-import unittest
+# encoding: utf-8
+import os
+import codecs
+import re
+import subprocess
 
-import cdcat
+import unittest
+import logging
+logging.basicConfig(
+    format='# %(levelname)s: %(message)s',
+    # level=logging.DEBUG,
+    level=logging.ERROR,
+)
+
+from cdcat.copy_helper import CopyHelper
+
+BASE_DIR = u'tests'
+DIRECTORY_OF_EXPECTED_SCRIPTS = u'expected'
+CATALOG_FILE_SELECTED = u'files_selected'
+CATALOG_FILE_NOT_SELECTED = u'no_selected_files'
 
 class TestCdcat(unittest.TestCase):
 
-    # def test_twenty_four_seven(self):
-    #     oh = OpeningHours('24/7')
-    #     self.assertEqual(oh.getStateString(), 'open')
-    #     self.assertTrue(oh.isWeekStable())
+    def test_compare_with_expected(self):
 
-    #     oh = OpeningHours('open')
-    #     self.assertEqual(oh.getStateString(), 'open')
-    #     self.assertTrue(oh.isWeekStable())
+        dir_of_expected_scripts = os.path.join(
+            BASE_DIR,
+            DIRECTORY_OF_EXPECTED_SCRIPTS
+        )
+        for dirname, dirnames, filenames in os.walk(dir_of_expected_scripts):
+            for filename in filenames:
+                suffix = filename.replace(CATALOG_FILE_SELECTED + '.', '')
+                suffix_re = re.match(
+                    r'(?P<media_id>[0-9]+)'
+                    r'\.(?P<method>[a-z-]+)'
+                    r'\.(?P<file_extention>[a-z]+)$',
+                    suffix
+                )
+                if not suffix_re:
+                    logging.warning("No match for {}".format(filename))
+                    continue
+                media_id = suffix_re.group('media_id')
+                # if media_id != '1':
+                #     continue
+                logging.info(filename)
 
-    def test_invalid_value(self):
-        True
-    #     self.assertRaises(ParseException, OpeningHours, 'fail "not valid keyword"')
-    #     self.assertRaises(ParseException, OpeningHours, ' ')
+                method = suffix_re.group('method')
+                file_extention = suffix_re.group('file_extention')
+                base_filename = os.path.join(BASE_DIR, CATALOG_FILE_SELECTED)
+                expected_script_filename = os.path.join(dirname, filename)
+
+                copy_helper = CopyHelper()
+                file_suffix_to_executing_shell = {v:k for k, v in copy_helper._executing_shell_to_file_suffix.items()}
+                copy_helper._executing_shell = file_suffix_to_executing_shell[file_extention]
+                for catalog_filename in [u'hcf', u'hcf.unp']:
+                    catalog_filename = base_filename + '.' + catalog_filename
+                    copy_helper.parse_catalog_file(
+                        catalog_filename,
+                        generate_format=method
+                    )
+                    generated_script_filename = '{}.{}.{}.{}'.format(
+                        copy_helper._base_filename,
+                        media_id,
+                        method,
+                        file_extention
+                    )
+                    try:
+                        output = subprocess.check_output(
+                            [
+                                'diff',
+                                generated_script_filename,
+                                expected_script_filename
+                            ]
+                        )
+                        if output:
+                            print output
+                    except subprocess.CalledProcessError as error:
+                        print error.output
+                        return False
 
 if __name__ == "__main__":
     unittest.main()
