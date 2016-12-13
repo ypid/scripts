ba3be3ea2872557106582fdbfacbe96f83d25c1d
Robin Schneider <ypid@riseup.net>
2016-12-13 20:59:13 +0100

    Add simple ftp server example

---
+++ ftpserver-cli.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# ftpserver-cli.py
+# Source: https://askubuntu.com/questions/17084/how-do-i-temporarily-run-an-ftp-server/17095#17095
+import sys
+import argparse
+
+from pyftpdlib.authorizers import DummyAuthorizer
+from pyftpdlib.handlers import FTPHandler
+from pyftpdlib.servers import FTPServer
+
+
+def processCmdLineOptions():
+    global optparser
+    optparser = argparse.ArgumentParser(description="ftpserver-cli",
+                                        formatter_class=argparse.RawDescriptionHelpFormatter)
+    optparser.add_argument('-u', '--username', action='store', type=str,
+                           default="user", help="username")
+    optparser.add_argument('-p', '--password', action='store', type=str,
+                           default="12345", help="password")
+    optparser.add_argument('-t', '--port', action='store', type=int,
+                           default="21", help="port")
+    optparser.add_argument('-d', '--directory', action='store', type=str,
+                           default="/home/stefano/Projekte/", help="port")
+    optargs = optparser.parse_args(sys.argv[1:])  # (sys.argv)
+    return optargs
+
+
+optargs = processCmdLineOptions()
+
+print(
+    "Using: user: %s pass: %s port: %d dir: %s" %
+     (optargs.username, optargs.password, optargs.port, optargs.directory))
+
+authorizer = DummyAuthorizer()
+authorizer.add_user(
+    optargs.username,
+     optargs.password,
+     optargs.directory,
+     perm="elradfmw")
+# authorizer.add_anonymous("/home/nobody")
+
+handler = FTPHandler
+handler.authorizer = authorizer
+
+server = FTPServer(("127.0.0.1", optargs.port), handler)
+server.serve_forever()
