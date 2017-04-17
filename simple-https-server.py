11aec6a04a2b14b3c04f676f321d8f4244bcecad
Robin Schneider <ypid@riseup.net>
2017-04-17 15:55:44 +0200

    Add simple-https-server

---
+++ simple-https-server.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+# Source: https://gist.github.com/JonathonReinhart/f26365364918b44d82bbd6b90269fbd6
+
+import BaseHTTPServer
+import SimpleHTTPServer
+import ssl
+import argparse
+
+ap = argparse.ArgumentParser()
+ap.add_argument('--certfile')
+ap.add_argument('--keyfile')
+ap.add_argument('--port', type=int, default=443)
+ap.add_argument('--hostname', default='localhost')
+args = ap.parse_args()
+
+url = 'https://' + args.hostname
+if args.port != 443:
+    url += ':' + str(args.port)
+url += '/'
+
+httpd = BaseHTTPServer.HTTPServer((args.hostname, args.port), SimpleHTTPServer.SimpleHTTPRequestHandler)
+httpd.socket = ssl.wrap_socket(httpd.socket, certfile=args.certfile, keyfile=args.keyfile, server_side=True)
+
+print 'Serving at', url
+httpd.serve_forever()
